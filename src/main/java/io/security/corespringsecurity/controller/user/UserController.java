df780aced14198033ea43738c13c95755ea2d077
hwiyeon0831 <aab555586@gmail.com>
2023-03-21 00:13:37 +0900

    3)-1 진도에 맞게 소스 수정 - 부가적인 설정 (DB 등) 삭제 - register, messages, mypage 페이지성 파일 추가 - 없었던 Controller 추가

---
+++ src/main/java/io/security/corespringsecurity/controller/user/UserController.java
@@ -0,0 +1,18 @@
+package io.security.corespringsecurity.controller.user;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class UserController {
+	@GetMapping(value="/mypage")
+	public String myPage() throws Exception {
+		return "user/mypage";
+	}
+
+	@GetMapping("/users")
+	public String createUser(){
+		return "user/login/register";
+	}
+
+}
