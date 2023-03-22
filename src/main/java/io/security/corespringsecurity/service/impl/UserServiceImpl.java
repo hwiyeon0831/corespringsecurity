e8aee637570fb4c5f20e648d8ac01d0104ddc243
hwiyeon0831 <aab555586@gmail.com>
2023-03-23 07:10:13 +0900

    3)-3 내용 등록 - JPA, DB 설정 - 계정 Domain 생성 - User 등록을 위한 createUser 메소드 생성 - UserRepository 생성

---
+++ src/main/java/io/security/corespringsecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package io.security.corespringsecurity.service.impl;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.repository.UserRepository;
+import io.security.corespringsecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void createUser(Account account) {
+		userRepository.save(account);
+	}
+}
