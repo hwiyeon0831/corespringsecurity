9d60f7dea4a4aa914ad89d23ab0906f76dbf7345
hwiyeon0831 <aab555586@gmail.com>
2023-03-23 06:34:16 +0900

    3)-2 내용 등록 - WebIgnore 설정

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity.security.configs;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -20,8 +22,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         //인메모리 방식으로 사용자 생성. 권한 부여.
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "MANAGER", "USER"); // 위 설정으로는 권한에 대한 계층 구조를 Spring Security에서 파악하지 못한다. -> 추후 실전 프로젝트에서 학습 예정
     }
 
     @Bean   //passwordEncoder는 빈으로 등록
@@ -30,6 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //permitAll()과 다른 점 : permitAll()은 해당 요청에 대해 허가는 하되 보안 필터는 거친다. WebIgnore은 명시한 자원에 대해 아예 보안 필터를 거치지 않도록 한다.
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
